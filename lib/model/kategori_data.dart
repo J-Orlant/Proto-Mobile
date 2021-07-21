2b1b7bf73594d8ca23be7d0e949bb4bfb3c9e057
Rainner-yobelgates <81410971+Rainner-yobelgates@users.noreply.github.com>
2021-07-21 17:09:25 +0700

    Update kategori_data.dart
    total ad 16, gw bkin udh bkin templetenya sesuai jumlah 34 klo ad yg main ngisi tinggl di isi ae yg kosong, mna nih yoges masih tdur pdhl mau ngerjain kwu

--- lib/model/kategori_data.dart
+++ lib/model/kategori_data.dart
@@ -75,3 +75,738 @@ var aceh = [
     nama: 'Gulai Terjun',
   ),
 ];
+
+var bali = [
+  Kategori(
+    gambar:
+        'https://www.resepistimewa.com/wp-content/uploads/ayam-betutu-bali.jpg',
+    nama: 'Ayam Betutu',
+  ),
+  Kategori(
+    gambar:
+        'https://sahabatnesia.com/wp-content/uploads/2020/06/makanan-khas-bali-Sate-Plecing.jpg',
+    nama: 'Sate Penyu',
+  ),
+  Kategori(
+    gambar:
+        'https://www.gotravela.id/wp-content/uploads/2020/06/Rujak-Bulung-1.png',
+    nama: 'Rujak Bulung',
+  ),
+  Kategori(
+    gambar:
+        'https://asset.kompas.com/crops/o0dGFr1gthyBRTfg9-2Jxy-oTOY=/0x0:1000x667/750x500/data/photo/2020/06/20/5eecf73cbbd07.jpg',
+    nama: 'Bebek Betutu',
+  ),
+];
+
+var jawa_barat = [
+  Kategori(
+    gambar:
+        'https://i2.wp.com/resepkoki.id/wp-content/uploads/2019/10/Resep-Karedok-small.jpg?fit=600%2C401&ssl=1',
+    nama: 'Karedok',
+  ),
+  Kategori(
+    gambar:
+        'https://www.masakapahariini.com/wp-content/uploads/2020/11/batagor-bandung-disajikan-500x300.jpg',
+    nama: 'Batagor',
+  ),
+  Kategori(
+    gambar:
+        'https://cdn-2.tstatic.net/solo/foto/bank/images/peuyeum-bandung_20170129_183516.jpg',
+    nama: 'Peuyeum',
+  ),
+  Kategori(
+    gambar:
+        'https://static.republika.co.id/uploads/images/inpicture_slide/surabi-solo-hotel-mulia-_140922052610-482.jpg',
+    nama: 'Surabi',
+  ),
+];
+
+var bengkulu = [
+  Kategori(
+    gambar:
+        'https://bisniswisata.co.id/wp-content/uploads/2020/12/20201212_122334.jpg',
+    nama: 'Pendap',
+  ),
+  Kategori(
+    gambar:
+        'https://cdn-2.tstatic.net/travel/foto/bank/images/bagar-hiu_20160817_155447.jpg',
+    nama: 'Bagar Hiu',
+  ),
+  Kategori(
+    gambar:
+        'https://i.pinimg.com/originals/65/2b/6c/652b6cd47a52e082f6bbbeae2120db62.jpg',
+    nama: 'Rebung Asam Undak Liling',
+  ),
+  Kategori(
+    gambar:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1553347594/xlztuwowc7xlvefiblrk.jpg',
+    nama: 'Lempuk Durian',
+  ),
+];
+
+var sumatra_barat = [
+  Kategori(
+    gambar:
+        'https://cdn-2.tstatic.net/tribunnews/foto/bank/images/resep-rendang.jpg',
+    nama: 'Rendang',
+  ),
+  Kategori(
+    gambar:
+        'https://asset.kompas.com/crops/rqGBRurZqX4_a5qY31V_4tQfdEo=/0x0:1000x667/750x500/data/photo/2021/03/28/60607a72127cd.jpg',
+    nama: 'Sate Padang',
+  ),
+  Kategori(
+    gambar:
+        'https://selerasa.com/wp-content/uploads/2020/02/Resep-soto-padang-1200x720.jpg',
+    nama: 'Soto Padang',
+  ),
+  Kategori(
+    gambar:
+        'https://cdn-2.tstatic.net/travel/foto/bank/images/lemang-tapai.jpg',
+    nama: 'Lamang Tapal',
+  ),
+];
+
+var sumatra_utara = [
+  Kategori(
+    gambar:
+        'https://www.masakapahariini.com/wp-content/uploads/2020/10/mie-gomak-780x440.jpg',
+    nama: 'Mie Gomak',
+  ),
+  Kategori(
+    gambar:
+        'https://primadaily.com/wp-content/uploads/2020/09/Resep-Enak-Babi-Panggang-Khas-Karo.jpg',
+    nama: 'Babi Panggang Karo',
+  ),
+  Kategori(
+    gambar:
+        'https://cdns.klimg.com/merdeka.com/i/w/news/2019/09/16/1109675/540x270/3-resep-soto-medan-mulai-soto-medan-daging-ayam-sampai-bening-rev-2.jpg',
+    nama: 'Soto Medan',
+  ),
+  Kategori(
+    gambar:
+        'https://cdns.klimg.com/merdeka.com/i/w/news/2020/05/20/1179161/540x270/gurih-dan-lembut-ini-kuliner-bihun-bebek-medan-yang-terkenal.jpg',
+    nama: 'Bihun Bebek',
+  ),
+];
+
+var riau = [
+  Kategori(
+    gambar:
+        'https://img-global.cpcdn.com/recipes/ed15369085afa765/1200x630cq70/photo.jpg',
+    nama: 'Ikan Baung Asam Pedas',
+  ),
+  Kategori(
+    gambar:
+        'https://img-global.cpcdn.com/recipes/01b5a101c1c503fa/751x532cq70/gulai-ikan-patin-foto-resep-utama.jpg',
+    nama: 'Gulai Ikan Patin',
+  ),
+  Kategori(
+    gambar:
+        'https://img-global.cpcdn.com/recipes/25fbdfeba9fe5d3c/1200x630cq70/photo.jpg',
+    nama: 'Gulai Belacan',
+  ),
+  Kategori(
+    gambar:
+        'https://www.ranahriau.com/foto_berita/29Resep-dan-Cara-Membuat-Roti-Jala.jpeg',
+    nama: 'Roti Jala',
+  ),
+];
+
+var kepulauan_riau = [
+  Kategori(
+    gambar:
+        'https://pdbifiles.nos.jkt-1.neo.id/files/2017/09/27/findayuliana_sambal-cencalok.jpg',
+    nama: 'Cencaluk',
+  ),
+  Kategori(
+    gambar:
+        'https://imgx.sonora.id/crop/0x0:0x0/x/photo/2020/08/24/4016275182.jpg',
+    nama: 'Roti Canai',
+  ),
+  Kategori(
+    gambar:
+        'https://cdn-2.tstatic.net/belitung/foto/bank/images/mi-lendir_20180427_210816.jpg',
+    nama: 'Mie Lendir',
+  ),
+  Kategori(
+    gambar:
+        'https://img.okezone.com/content/2017/07/14/298/1736390/punya-ikan-selais-riau-di-rumah-masak-sambal-balado-yang-pedasnya-nendang-yuk-naQStClONJ.jpg',
+    nama: 'Ikan Selais Asap',
+  ),
+];
+
+var sumatra_selatan = [
+  Kategori(
+    gambar:
+        'https://asset.kompas.com/crops/LPTbmb14tDCmSay2EAvNihccDv0=/0x47:909x653/750x500/data/photo/2020/11/20/5fb76c4aa04bc.jpg',
+    nama: 'Pempek',
+  ),
+  Kategori(
+    gambar:
+        'https://cdn.idntimes.com/content-images/post/20200530/2-a6cb06028f22e293879ec7f5cc4a19fd.jpg',
+    nama: 'Pindang',
+  ),
+  Kategori(
+    gambar:
+        'https://asset.kompas.com/crops/Qu-uGRiw6A4NINs_6VhIZBrvp1I=/0x386:667x831/750x500/data/photo/2021/05/12/609b4a1d9af08.jpg',
+    nama: 'Kemplang',
+  ),
+  Kategori(
+    gambar:
+        'https://asset-a.grid.id/crop/0x0:0x0/x/photo/2020/06/28/3505734671.jpg',
+    nama: 'Tekwan',
+  ),
+];
+
+var kepulauan_bangka_belitung = [
+  Kategori(
+    gambar:
+        'https://i1.wp.com/resepkoki.id/wp-content/uploads/2017/07/Resep-Tenggiri-lempah-kuning-2.jpg?fit=600%2C600&ssl=1',
+    nama: 'Lempah Kuning',
+  ),
+  Kategori(
+    gambar: 'https://statik.tempo.co/data/2016/03/08/id_488008/488008_620.jpg',
+    nama: 'Sup Gangan',
+  ),
+  Kategori(
+    gambar:
+        'https://cdn-2.tstatic.net/jabar/foto/bank/images/laksa-dabo_20170403_193746.jpg',
+    nama: 'Lakse',
+  ),
+  Kategori(
+    gambar:
+        'https://cdn.idntimes.com/content-images/community/2019/06/a60b6f4c-d8e1-45d4-a741-10e9d0f5f046-86b546f7115dce81870f626fa1dba892_600x400.jpeg',
+    nama: 'Berego.',
+  ),
+];
+
+var lampung = [
+  Kategori(
+    gambar:
+        'https://asset.kompas.com/crop/116x0:1018x601/750x500/data/photo/2019/05/03/3776094973.jpg',
+    nama: 'Seruit',
+  ),
+  Kategori(
+    gambar:
+        'https://i.ytimg.com/vi/KR8m3ivkb9k/maxresdefault.jpg',
+    nama: 'Gabing',
+  ),
+  Kategori(
+    gambar:
+        'https://mesinvacuumfrying.id/wp-content/uploads/2019/05/gedhang.jpg',
+    nama: 'Keripik Pisang',
+  ),
+  Kategori(
+    gambar:
+        'https://www.rumahmesin.com/wp-content/uploads/2016/01/ingin-tahu-cara-membuat-keripik-nangka-ini-langkahnya.jpg',
+    nama: 'Keripik Nangka',
+  ),
+];
+
+var banten = [
+  Kategori(
+    gambar:
+        'https://lh6.googleusercontent.com/-cM1Ur9pgtwo/VQwjwBy9YyI/AAAAAAAADwU/6tv_F6zRPgQ/s640/blogger-image--2110022003.jpg',
+    nama: 'Angeun Lada',
+  ),
+  Kategori(
+    gambar:
+        'https://indonesia.go.id/assets/upload/headline//1606304466_rabeg.jpeg',
+    nama: 'Rabeg',
+  ),
+  Kategori(
+    gambar:
+        'https://awsimages.detik.net.id/community/media/visual/2019/10/31/d4fa4a3e-b4e6-40be-bb89-60f04d8599d1.jpeg?w=700&q=90',
+    nama: 'Sate Bebek',
+  ),
+  Kategori(
+    gambar:
+        'https://assets.pikiran-rakyat.com/crop/0x0:0x0/750x500/photo/kabarbanten/2019/05/ketan-bintul.jpg',
+    nama: 'Ketan Bintul',
+  ),
+];
+
+var jawa_tengah = [
+  Kategori(
+    gambar:
+        'https://www.masakapahariini.com/wp-content/uploads/2019/09/shutterstock_247169467.jpg',
+    nama: 'Lumpia',
+  ),
+  Kategori(
+    gambar:
+        'https://asset.kompas.com/crops/exjVaZkpLcXM4kBo6dh1t6DQmfU=/0x46:400x313/750x500/data/photo/2021/03/11/6049a3af14f01.jpg',
+    nama: 'Cabuk rambak',
+  ),
+  Kategori(
+    gambar:
+        'https://asset.kompas.com/crops/W8TZoQkOT1J9dKBNSeu8it_cyMY=/0x0:698x465/750x500/data/photo/2021/03/16/605056d495ba4.jpg',
+    nama: 'Getuk Goreng',
+  ),
+  Kategori(
+    gambar:
+        'https://media.suara.com/pictures/970x544/2020/08/30/50542-ilustrasi-kue-leker-pisang-pixabayandres-salazar.jpg',
+    nama: 'Kue Leker',
+  ),
+];
+
+var yogyakarta = [
+  Kategori(
+    gambar:
+        'https://id-test-11.slatic.net/p/43be9e051ca9cf33c77cf39ef47a7c8d.jpg_720x720q80.jpg_.webp',
+    nama: 'Bakpia Pathok',
+  ),
+  Kategori(
+    gambar:
+        'https://www.pegipegi.com/travel/wp-content/uploads/2016/05/sate-klatak.jpg',
+    nama: 'Sate Klatak',
+  ),
+  Kategori(
+    gambar:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1575771963/bvtatye6xui7sbhvf87o.jpg',
+    nama: 'Yangko',
+  ),
+  Kategori(
+    gambar:
+        'https://doyanresep.com/wp-content/uploads/2020/02/resep-oseng-mercon.jpg',
+    nama: 'Oseng Oseng Mercon',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
+
+var provinsi = [
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+  Kategori(
+    gambar:
+        '',
+    nama: '',
+  ),
+];
